a0bd8c158a6da27637ff013669d8906f3b44ce98
Amaan Al Mir <amaanalmir@gmail.com>
2025-12-29 02:16:04 +0400

    added few new shadcn comps

--- app/(main)/access/users/page.tsx
+++ app/(main)/access/users/page.tsx
@@ -1,6 +1,6 @@
 import { Users } from "lucide-react";
 
-export default function UsersPage() {
+export default async function UsersPage() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div>
